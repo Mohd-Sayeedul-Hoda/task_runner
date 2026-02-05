aff1c14b9532f79a9dc792a8ff1ed15d25b9ad12
Sayeed <sayeedulhoda@gmail.com>
2026-02-05 23:59:47 +0530

    added schema and query for scheduler

---
+++ sql/queries/task.sql
@@ -0,0 +1,35 @@
+-- name: CreateTask :one
+INSERT INTO tasks(id, task_type, payload, status,  run_at, created_at, updated_at)
+VALUES( 
+  gen_random_uuid(),
+  $1,
+  $2,
+  $3,
+  $4,
+  NOW(),
+  NOW()
+)
+RETURNING *;
+
+-- name: GetTasksByStatus :many
+SELECT * FROM tasks
+WHERE status = $1 
+ORDER BY run_at DESC;
+
+-- name: UpdateTaskStatus :exec
+UPDATE tasks
+SET status = $1, updated_at = NOW()
+WHERE id = $2;
+
+-- name: GetPendingTasksForUpdate :many
+SELECT * FROM tasks
+WHERE status = 'PENDING' 
+  AND run_at <= NOW()
+ORDER BY run_at ASC
+LIMIT $1
+FOR UPDATE SKIP LOCKED;
+
+-- name: GetStalledTasks :many
+SELECT * FROM tasks
+WHERE status IN ('RUNNING', 'QUEUED')
+AND updated_at < NOW() - INTERVAL 5 MINUTE;
---
+++ sql/schema/20260204113002_tasks.sql
@@ -0,0 +1,19 @@
+-- +goose Up
+-- +goose StatementBegin
+CREATE TABLE IF NOT EXISTS tasks(
+  id UUID PRIMARY KEY,
+  task_type TEXT NOT NULL,
+  payload JSONB,
+  status TEXT NOT NULL,
+  run_at TIMESTAMP NOT NULL,
+  created_at TIMESTAMP NOT NULL DEFAULT NOW(),
+  updated_at TIMESTAMP NOT NULL DEFAULT NOW()
+);
+CREATE INDEX idx_tasks_status_run_at
+ON tasks(status,
+run_at);
+-- +goose StatementEnd
+-- +goose Down
+-- +goose StatementBegin
+DROP TABLE IF EXISTS tasks;
+-- +goose StatementEnd
